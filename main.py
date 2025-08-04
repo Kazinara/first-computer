6b09b817dccb5fd39620ce9021efa38daecfd89f
Kazinara <anastasiakorolevskaya02@gmail.com>
2025-08-04 19:10:23 +0300

    изменила функцию get_text

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
 from gtts import gTTS
 import pdfplumber
-file_path="text.pdf"
-with pdfplumber.PDF(open(file_path, mode='rb')) as pdf_file:
-    pages = ''.join([page.extract_text() for page in pdf_file.pages]).replace('\n',' ')
-    def get_txt():
-        pdf_file_ = pages    
-        return pdf_file_
+
+
+def get_txt():
+    file_path="text.pdf"
+    with pdfplumber.PDF(open(file_path, mode='rb')) as pdf_file:
+        pages = ''.join([page.extract_text() for page in pdf_file.pages]).replace('\n',' ')
+    return pages
     
-    text_one = get_txt()
-    print(text_one)
-
-
-    def get_sound(txt):
-        language = 'ru'
-        voiced_txt = gTTS(text=txt, lang=language, slow=False)
-        voiced_txt.save('voiced_text.mp3')
+    
+def get_sound(txt):
+    language = 'ru'
+    voiced_txt = gTTS(text=txt, lang=language, slow=False)
+    voiced_txt.save('voiced_text.mp3')
 
 
 def main():
-    get_txt()
-    get_sound(text_one)
+    pdf_text = get_txt()
+    print(pdf_text)
+    get_sound(pdf_text)
 
 
 if __name__ =='__main__':
